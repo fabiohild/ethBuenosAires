a62d77d5b38f066ac972802a78d3fdfdaf10774e
Martin Diz <gitlab@martindiz.com.ar>
2018-05-27 01:31:21 -0300

    show tokens table

--- mytokens.js
+++ mytokens.js
@@ -1,12 +1,11 @@
+
   function test(){
 
 var myAddress = web3.eth.coinbase
-// var transactionObject = {
-//     from: myAddress,
-//     gas: 900000,
-///     gasPrice: 3000000000
-  // };
   let tokens = []
+  let table = '<h2>user mementos</h2><p><table ><tbody id="mementos"><tr><th>Icon</th><th>name</th> <th>description</th></tr></tbody></table>'
+  $('#mementos').html("");
+  $('#mementos').append(table);
   NFTContract.balanceOf.call(myAddress,(error, result) => {
     if (error) console.log(error)
     else {
@@ -20,7 +19,28 @@ var myAddress = web3.eth.coinbase
               else {
               console.log(uri)
               tokens.push(uri)
-              }
+              //request({url:uri},function (error, response, body) {
+              $.getJSON( uri, function(json) { // console.log(result) })
+                //     console.log('error:', error); 
+                //     console.log('statusCode:', response && response.statusCode); 
+                //     console.log("le body:", body);
+                    console.log(json)
+                    var row = document.createElement('tr');
+                    var col = document.createElement('td');
+                    var col2 = document.createElement('td');
+                    var col3 = document.createElement('td');
+                    row.appendChild(col); 
+                    row.appendChild(col2);
+                    row.appendChild(col3);
+                    console.log('Using the following image');
+                    console.log(json['image']);
+                    col.innerHTML = '<div><img class="u-max-full-width" src="'+json['image']+'" alt=""></img></div>'
+                    col2.innerHTML = json['name'];
+                    col3.innerHTML = json['description']; 
+                    var table = document.getElementById("mementos"); 
+                    table.appendChild(row);
+                });
+            }
             });
           }
         })
@@ -30,6 +50,8 @@ var myAddress = web3.eth.coinbase
   // NFTContract.totalSupply.call( (error, result) => {console.log(result)
   //  })
   }
+
+  test();
 
 
 
