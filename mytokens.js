74931919b78514071d3bc31fa4a82253ef1cd632
fabiohild <fabiohildebrand@gmail.com>
2018-05-27 02:30:23 -0300

    mytokens

--- mytokens.js
+++ mytokens.js
@@ -1,42 +1,29 @@
-
-
 var myAddress = web3.eth.coinbase
-  let tokens = []
-  let table = '  <div id="mementos" class="row"></div>'//  <h2>user mementos</h2><p><table ><tbody id="mementos"><tr><th>Icon</th><th>name</th> <th>description</th></tr></tbody></table>'
-  $('#mementos').html("");
-  $('#mementos').append(table);
-  NFTContract.balanceOf.call(myAddress,(error, result) => {
-    if (error) console.log(error)
-    else {
-      console.log("Total amount of collectibles:",result.valueOf())
-      for (i = 0; i < result; i++) {
-        NFTContract.tokenOfOwnerByIndex.call(myAddress,i, (error, idx) => {
-          if (error) console.log(error)
-          else {
-            NFTContract.tokenURI.call(idx, (error, uri) => {
-              if (error) console.log(error)
-              else {
-              console.log(uri)
-              tokens.push(uri)
-              $.getJSON( uri, function(json) { 
-                let card = '<div class="one-third column" style="margin-top: 0%">\
-                    <img class="u-max-full-width" src="'+json['image']+'">\
-                    <h4>'+json['name']+'</h4>\
-                    <p>'+json['description']+'</p>\
-                    </div>'   
-                    var deck = document.getElementById("mementos"); 
-                    deck.insertAdjacentHTML( 'beforeend', card );
-                });
+//  <h2>user mementos</h2><p><table ><tbody id="mementos"><tr><th>Icon</th><th>name</th> <th>description</th></tr></tbody></table>'
+NFTContract.balanceOf.call(myAddress, (error, result) => {
+  if (error) console.log(error)
+  else {
+    console.log("Total amount of collectibles:", result.valueOf())
+    for (i = 0; i < result; i++) {
+      NFTContract.tokenOfOwnerByIndex.call(myAddress, i, (error, idx) => {
+        if (error) console.log(error)
+        else {
+          NFTContract.tokenURI.call(idx, (error, uri) => {
+            if (error) console.log(error)
+            else {
+              $.getJSON(uri, function (json) {
+                let card = '<div class="col-sm-4" style="margin-top: 0%">\
+                    <img class="img-fluid img-thumbnail rounded" src="' + json['image'] + '">\
+                    <br><br><h3 class="text-center">' + json['name'] + '</h3>\
+                    <p>' + json['description'] + '</p><br>\
+                    </div>'
+                var deck = document.getElementById("mementos");
+                deck.insertAdjacentHTML('beforeend', card);
+              });
             }
-            });
-          }
-        })
-      }
+          });
+        }
+      })
     }
-  });
-  
-
-
-
-
-
+  }
+});
