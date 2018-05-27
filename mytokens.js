2eac44574812e9e0d7497aa12cb9bb3326bad4ef
Martin Diz <gitlab@martindiz.com.ar>
2018-05-27 02:09:57 -0300

    Change table for div. Removed test function

--- mytokens.js
+++ mytokens.js
@@ -1,9 +1,8 @@
 
-  function test(){
 
 var myAddress = web3.eth.coinbase
   let tokens = []
-  let table = '<h2>user mementos</h2><p><table ><tbody id="mementos"><tr><th>Icon</th><th>name</th> <th>description</th></tr></tbody></table>'
+  let table = '  <div id="mementos" class="row"></div>'//  <h2>user mementos</h2><p><table ><tbody id="mementos"><tr><th>Icon</th><th>name</th> <th>description</th></tr></tbody></table>'
   $('#mementos').html("");
   $('#mementos').append(table);
   NFTContract.balanceOf.call(myAddress,(error, result) => {
@@ -19,26 +18,14 @@ var myAddress = web3.eth.coinbase
               else {
               console.log(uri)
               tokens.push(uri)
-              //request({url:uri},function (error, response, body) {
-              $.getJSON( uri, function(json) { // console.log(result) })
-                //     console.log('error:', error); 
-                //     console.log('statusCode:', response && response.statusCode); 
-                //     console.log("le body:", body);
-                    console.log(json)
-                    var row = document.createElement('tr');
-                    var col = document.createElement('td');
-                    var col2 = document.createElement('td');
-                    var col3 = document.createElement('td');
-                    row.appendChild(col); 
-                    row.appendChild(col2);
-                    row.appendChild(col3);
-                    console.log('Using the following image');
-                    console.log(json['image']);
-                    col.innerHTML = '<div><img class="u-max-full-width" src="'+json['image']+'" alt=""></img></div>'
-                    col2.innerHTML = json['name'];
-                    col3.innerHTML = json['description']; 
-                    var table = document.getElementById("mementos"); 
-                    table.appendChild(row);
+              $.getJSON( uri, function(json) { 
+                let card = '<div class="one-third column" style="margin-top: 0%">\
+                    <img class="u-max-full-width" src="'+json['image']+'">\
+                    <h4>'+json['name']+'</h4>\
+                    <p>'+json['description']+'</p>\
+                    </div>'   
+                    var deck = document.getElementById("mementos"); 
+                    deck.insertAdjacentHTML( 'beforeend', card );
                 });
             }
             });
@@ -47,12 +34,7 @@ var myAddress = web3.eth.coinbase
       }
     }
   });
-  // NFTContract.totalSupply.call( (error, result) => {console.log(result)
-  //  })
-  }
-
-  test();
-
+  
 
 
 
